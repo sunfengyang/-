7c1a626c4d24a8ca2e2fb8be0777bda2f0c73f2a
sunfengyang <363257617@qq.com>
2019-09-10 22:56:28 +0800

    koa2基本

--- KOA2/9_ejs.js
+++ KOA2/9_ejs.js
@@ -1,2 +1,22 @@
-2135454
-gogogo
+const Koa = require('koa');
+const app = new Koa();
+const views = require('koa-views');
+const path = require('path');
+
+//加载模板
+app.use(views(path.join(__dirname,'.views'),{  //文件当前路径
+    extension : 'ejs'
+}))
+
+app.use(async ctx=>{
+    let title = 'hello';
+    await ctx.render('index',{
+        title,
+        list:[
+            {name:'html',age:20},
+            {name:'css',age:30},
+            {name:'js',age:40},
+            {name:'koa2',age:50},
+        ]
+    })
+});
